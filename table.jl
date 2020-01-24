317cc737c184b85b1a49a0e35242ec750623f35c
Miguel Rust <hava12soft@gmail.com>
2020-01-23 17:32:23 -0700

    Cleaned up output

--- table.jl
+++ table.jl
@@ -13,13 +13,15 @@ b = rand(1:500, 100)
 # Each non labeled parameter in the table function is a column
 # Columns must be single dimensional arrays
 
+println("Unnamed Table:")
 t = table(a, b)
 println(t)
 
 # You can optionally declare column names and a private key
 # The private key is the default column used for sorting and is used in joins
 
-namedTable = (a, b, names=[:id, :val], pkey=:id)
+println("Named Table:")
+namedTable = table(a, b, names=[:id, :val], pkey=:id)
 println(namedTable)
 
 # In Julia there are mutating and non mutating functions
@@ -29,18 +31,27 @@ println(namedTable)
 # Both of these sorts are sorting by the private key in ascending order
 
 # Non mutating sort of table
+
+println("Sorted Table Output")
 println(sort(namedTable))
+println("Proof that it did not mutate table:")
 println(namedTable)
 
 # Mutating sort of table
-println(sort!(namedTable))
-println(namedTable)
+# Mutating sort cannot be performed on a table with a private key
+println("Mutating Sort Output:")
+println(sort!(t))
+println("Proof it did mutate table:")
+println(t)
 
 # Sort by private key descending order
+println("Table sorted by private key in descending order:")
 println(sort(namedTable, rev=true))
 
 # Sort by column other than private key
+println("Table sorted by val:")
 println(sort(namedTable, :val))
 
-# Retrive value by private key
+# Retrive row by private key
+println("Row retrived from table:")
 println(namedTable[3])
